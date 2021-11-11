02d125d872ef883b07a02570fe86441af84fb5ef
Akseli Suutari <akseli.suutari@hotmail.com>
2021-11-11 23:44:38 +0200

    Update PlaneSimulation.m
    Melkein toimii, rivin ensimmäiset jäävät asettamatta

--- PlaneSimulation.m
+++ PlaneSimulation.m
@@ -1,40 +1,55 @@
 %% PlaneSimulation
 
 
-x = [1, 2, 3, 5, 6, 7, 8, 9, 10,...
-    11, 12, 13, 14, 15, 16, 17 ,18, 19, 20, 21];
+x = 1:24;
 
 
 I = seatToInd(x)
-
+I = I';
 
 seats_in_row = 6;
 
 t_step = 1;
-i = 1;
+ii = 1;
 t_tot = 0;
 
-plane = zeros(max(x(:,1)), seats_in_row);
+plane = zeros(max(I(:,1)), seats_in_row);
+queue = [];
 
-while i<=length(x)
-   row_1 = x(i,1);
-   pos_1 = x(i,2);
+% TODO:
+% Changing row doesnt work at the moment, the first guy can't
+% get to his spot. 
+while ii<=length(I)
+   queue(end+1,:) = I(ii,:);
    
-   if row_1 == i
-      plane(row_1,pos_1) = 1; 
-      i = i + 1;
-      continue
+   
+   % Check if the first in queue is not on his row
+   % If not, add another person to queue and time
+   if queue(end,1) < size(queue,1)
+        ii = ii + 1;
+        t_tot = t_tot + t_step;
+        pause
+        continue
    end
+   % Stop queue and put all people on correct
+   % rows to their places
+   for jj=1:size(queue,1)
+       
+       if queue(jj,1) == jj
+          queue(jj,1)
+          queue(jj,2)
+          plane(queue(jj,1),queue(jj,2)) = 1
+          queue(jj,:) = 0;
+          
+          
+       end
+   end
+   queue( all(~queue,2), : ) = [];
+   ii = ii + 1;
    
    
 end
 
-
-
-
-
-
-%% Funkkarit
 
 
 % Noi on nyt rakennettu ks. jarjestyksella ja vaakasuoralla palautuksella:
@@ -45,14 +60,14 @@ end
 
 % Kaannetaan jos halutaan vaihtaa tarkastelu esim pystysuoraksi.
 
-function I = seatToInd(L);
+function I = seatToInd(L)
 % Converts the vector of seat numbers into a 2-dimensional vector of indeces.
     C = ceil(L./6);     % Counts the column from the seat number [1, n]
     R = mod(L-1, 6)+1;  % Counts the row from the seat number [1, 6]
     I = [C; R];
 end
 
-function N = indToSeat(V);
+function N = indToSeat(V)
 % Converts the vector of indeces into a 1-dimensional vector of seat numbers.
     N = (V(1,:) - 1)*6 + V(2,:);  % Counts the seat number from given indices
 
