97ed1252d12d0ae741bf7c68ceeb6b32a41fa612
Ville-Petteri <villepetteri.manninen@gmail.com>
2021-11-12 19:11:26 +0200

    Prototype 1, palauttaa while loopin kierrokset ja tulostaa lopputilanteen.

--- PlaneSimulation.m
+++ PlaneSimulation.m
@@ -1,57 +1,74 @@
 %% PlaneSimulation
+clc
 
+seats_in_row = 6;                               % Penkkien maara rivilla
+rows_in_plane = 20;                             % Rivien maara koneessa 
 
-x = [1, 2, 3, 5, 6, 7, 8, 9, 10,...
-    11, 12, 13, 14, 15, 16, 17 ,18, 19, 20, 21];
+line = [1:1:seats_in_row*rows_in_plane]';       % generoitu jono
 
+% line = line(randperm(length(line)))       % Talla komennolla saa
+                                            % randomoitua jarjestyksen
 
-I = seatToInd(x)
+time = planeBoarding(line, seats_in_row, rows_in_plane)  % simulaation aloitus
 
-
-seats_in_row = 6;
-
-t_step = 1;
-i = 1;
-t_tot = 0;
-
-plane = zeros(max(x(:,1)), seats_in_row);
-
-while i<=length(x)
-   row_1 = x(i,1);
-   pos_1 = x(i,2);
-   
-   if row_1 == i
-      plane(row_1,pos_1) = 1; 
-      i = i + 1;
-      continue
-   end
-   
-   
-end
-
-% testi
+% plane = zeros(rows_in_plane, seats_in_row);
+% plane(I(end, 1), I(end, 2)) = indToSeat(I(end,:), seats_in_row)
 
 %% Funkkarit
 
+function time = planeBoarding(line, seats, rows) 
+    % setuppia
+    time = 0;
+    plane = zeros(rows, seats);
+    aisle = zeros(rows, 2);
 
-% Noi on nyt rakennettu ks. jarjestyksella ja vaakasuoralla palautuksella:
-% 1, 7, 13, ...
-% 2, 8, ...
-% 3, 9,
-% ..
-
-% Kaannetaan jos halutaan vaihtaa tarkastelu esim pystysuoraksi.
-
-function I = seatToInd(L);
-% Converts the vector of seat numbers into a 2-dimensional vector of indeces.
-    C = ceil(L./6);     % Counts the column from the seat number [1, n]
-    R = mod(L-1, 6)+1;  % Counts the row from the seat number [1, 6]
-    I = [C; R];
+    % Muunnetaan jono vektori sisaltamaan indeksit
+    lineIn = seatToInd(line, seats);
+    
+    % Main Run: kaydaann lapi niin kauan kun joko jonossa tai kaytavalla on
+    % ihmisia.
+    while (any(any(lineIn)) || any(any(aisle)));
+        % Kaydaan lapi kaytava alkaen koneen lopusta
+        for i = rows:-1:1
+            person = aisle(i,:);
+            % Tehdaan toimenpide jonottavalle henkilolle
+            if(person(1) == i)             
+                % - Tarkistetaan onko henkilo oikealla rivilla
+                % - Asetetaan henkilo paikalleen
+                % - Poistetaan henkilo kaytavalta
+                plane(person(1), person(2)) = indToSeat(person, seats);
+                aisle(i,:) = [0, 0];
+            elseif(i ~= 20);
+                if (aisle(i + 1, 1) == 0);
+                % - Jos han ei ole oikealla rivilla -> siirretaan eteenpain
+                % - Siirretaan henkilo eteenpain
+                    aisle(i+1,:) = person;
+                    aisle(i,:) = [0, 0];
+                end
+            end
+        end
+        % Jos kaytavan ensimmainen paikka on tyhja niin jonottaja ulkoa
+        % voi tulla paastetaan kaytavalle. Poistetaan kokonainen sarake
+        % ulkojonosta.
+        if(any(any(lineIn)) && aisle(1,1) == 0)
+            aisle(1,:) = lineIn(1,:);
+            lineIn(1,:) = [];
+        end
+        time = time +1;
+    end
+    plane
 end
 
-function N = indToSeat(V);
-% Converts the vector of indeces into a 1-dimensional vector of seat numbers.
-    N = (V(1,:) - 1)*6 + V(2,:);  % Counts the seat number from given indices
+function I = seatToInd(L, cn);
+% Converts the vector of seat numbers into a 2-dimensional vector of indeces.
+    R = ceil(L./cn);     % Counts the row position from the seat number [1, 6]
+    C = mod(L-1, cn)+1;  % Counts the row in plane from the seat number [1, n]
+    I = [R, C];
+end
 
+function N = indToSeat(V, cn);
+% Converts the vector of indeces into a 1-dimensional vector of seat numbers.
+    N = (V(:,1) - 1)*cn + V(:,2);  % Counts the seat number from given indices
+    N = N(1);
 end
 
