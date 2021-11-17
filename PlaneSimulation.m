976c101d508c518bd0d41898fef1f20c1112a5fe
Akseli Suutari <akseli.suutari@hotmail.com>
2021-11-17 15:47:12 +0200

    Update PlaneSimulation.m
    Pyöritelty analyysia

--- PlaneSimulation.m
+++ PlaneSimulation.m
@@ -1,17 +1,75 @@
 %% PlaneSimulation
+clc, clearvars, close all
+
+seats_in_row = 6;                               % Penkkien maara rivilla
+rows_in_plane = 20;                             % Rivien maara koneessa 
 
 
-x = 1:24;
 
-x = x(randperm(length(x))); % Shuffle seats
+line = [1:1:seats_in_row*rows_in_plane]';       % generoitu jono
 
-I = seatToInd(x)
+tic
+n = 100000;
+time = [];
+for i = 1:n
+    line = line(randperm(length(line)));       % Talla komennolla saa
+                                            % randomoitua jarjestyksen
+    time(i) = planeBoarding(line, seats_in_row, rows_in_plane);  % simulaation aloitus
+end
+histogram(time)
+title('Random')
+m = mean(time);
+subtitle(['Mean: ',num2str(m,'%.2f')])
+toc
 
-time = planeBoarding(I)
 
 
 
 %% Funkkarit
+
+function time,room_made = planeBoarding(line, seats, rows) 
+    % setuppia
+    time = 0;
+    plane = zeros(rows, seats);
+    room_made = plane;
+    aisle = zeros(rows, 2);
+
+    % Muunnetaan jono vektori sisaltamaan indeksit
+    lineIn = seatToInd(line, seats);
+    
+    % Main Run: kaydaann lapi niin kauan kun joko jonossa tai kaytavalla on
+    % ihmisia.
+    while (any(any(lineIn)) || any(any(aisle)));
+        % Kaydaan lapi kaytava alkaen koneen lopusta
+        for i = rows:-1:1
+            person = aisle(i,:);
+            % Tehdaan toimenpide jonottavalle henkilolle
+            if(person(1) == i)             
+                % - Tarkistetaan onko henkilo oikealla rivilla
+                % - Asetetaan henkilo paikalleen
+                % - Poistetaan henkilo kaytavalta
+                plane(person(1), person(2)) = indToSeat(person, seats);
+                aisle(i,:) = [0, 0];
+            elseif(i ~= 20);
+                if (aisle(i + 1, 1) == 0);
+                % - Jos han ei ole oikealla rivilla -> siirretaan eteenpain
+                % - Siirretaan henkilo eteenpain
+                    aisle(i+1,:) = person;
+                    aisle(i,:) = [0, 0];
+                end
+            end
+        end
+        % Jos kaytavan ensimmainen paikka on tyhja niin jonottaja ulkoa
+        % voi tulla paastetaan kaytavalle. Poistetaan kokonainen sarake
+        % ulkojonosta.
+        if(any(any(lineIn)) && aisle(1,1) == 0)
+            aisle(1,:) = lineIn(1,:);
+            lineIn(1,:) = [];
+        end
+        time = time +1;
+    end
+    plane
+end
 
 
 % Noi on nyt rakennettu ks. jarjestyksella ja vaakasuoralla palautuksella:
@@ -22,60 +80,22 @@ time = planeBoarding(I)
 
 % Kaannetaan jos halutaan vaihtaa tarkastelu esim pystysuoraksi.
 
-function I = seatToInd(L)
+
+
+function I = seatToInd(L, cn);
+
 % Converts the vector of seat numbers into a 2-dimensional vector of indeces.
-    C = ceil(L./6);     % Counts the column from the seat number [1, n]
-    R = mod(L-1, 6)+1;  % Counts the row from the seat number [1, 6]
-    I = [C; R];
+    R = ceil(L./cn);     % Counts the row position from the seat number [1, 6]
+    C = mod(L-1, cn)+1;  % Counts the row in plane from the seat number [1, n]
+    I = [R, C];
 end
 
-function N = indToSeat(V)
+
+
+
+function N = indToSeat(V, cn);
 % Converts the vector of indeces into a 1-dimensional vector of seat numbers.
-    N = (V(1,:) - 1)*6 + V(2,:);  % Counts the seat number from given indices
+    N = (V(:,1) - 1)*cn + V(:,2);  % Counts the seat number from given indices
+    N = N(1);
 
-end
-
-
-function time = planeBoarding(lineIn)
-    
-% setuppia
-    time = 0;
-    rows = max(lineIn(1,:));
-    seat = max(lineIn(2,:));
-    seats = zeros(rows, seat);
-    aisle = zeros(rows, 2)';
-    
-    % Main Run: käydään läpi niin kauan kun joko jonossa tai käytävällä on
-    % ihmisiä.
-    while (any(any(lineIn)) || any(any(aisle)))
-        aisle
-        %lineIn
-        
-        % Käydään läpi käytävä alkaen koneen lopusta
-        for i = rows:-1:1
-            r = aisle(1,i);
-            s = aisle(2,i);
-            % Jos käytävä jonottaja on omalla rivillä niin lisää kaveri
-            % paikalleen ja poista käytävästä. Muussa tapauksessa jos
-            % jonottajan edessä ei ole ketään, niin hän voi siirtyä
-            % eteenpäin.
-            if(r == i) 
-                seats(r,s) = 1;
-                aisle(:,i) = [0;0];
-            elseif(r && aisle(1,i+1) == 0)
-                aisle(:,i+1) = [r;s];
-                aisle(:,i) = [0;0];
-            end
-        end
-        % Jos käytävän ensimmäinen paikka on tyhjä, niin jonottaja ulkoa
-        % voi tulla käytävä jonoon. Poistetaan kokonainen sarake
-        % ulkojonosta
-        
-        if(any(any(lineIn)) && aisle(1,1) == 0)
-            aisle(:,1) = lineIn(:,1);
-            lineIn(:,1) = [];
-        end
-        time = time +1;
-    end
-    seats'
 end
