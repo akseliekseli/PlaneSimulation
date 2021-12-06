9ddaa15e3888deb63bef5118e4a112ad573e059c
Ville-Petteri <villepetteri.manninen@gmail.com>
2021-12-06 13:08:17 +0200

    Rakennettu suhteellisen havainnollinen, mutta ei erityisen graafisesti miellyttävä animaatio. Animaatio on tällä hetkellä vaakasuuntaan, voin kääntää mikäli haluatte.

---
+++ planeAnimation.m
@@ -0,0 +1,105 @@
+function [time varargout] = planeAnimation(line, seats, rows, varargin)
+    switch nargin
+        case 5
+            time_step = varargin{1};
+            random = varargin{2};
+        case 4
+            time_step = varargin{1};
+            random = zeros(rows,1);
+        otherwise
+            time_step = 1;
+            random = zeros(rows,1);
+    end
+    % setuppia
+    time = 0;
+    plane = zeros(rows, seats);
+    aisle = zeros(rows, 3);
+    odotus = zeros(rows, 3);
+    wait_map = plane;
+    % Muunnetaan jono vektori sisaltamaan indeksit
+    lineIn = seatToInd(line, seats);
+    lineIn(:,3) = random';
+    % Main Run: Kaydaan lapi niin kauan kun joko jonossa tai kaytavalla on
+    % ihmisia.
+    if (mod(seats, 2) == 0)
+        figure(5)
+        hold on
+        axis equal
+        plot([0 21], [-1 -1], 'k');
+        plot([0 21], [-8 -8], 'k');
+        plot([21 21], [-1, -4], 'k');
+        plot([21 21], [-5 -8], 'k');
+        people = plot(1, -4.5, 'ko');
+        plot([0 21], [-4 -4], 'k');
+        plot([0 21], [-5 -5], 'k');
+        while (any(any(lineIn)) || any(any(aisle)))
+            % Kaydaan lapi kaytava alkaen koneen lopusta
+            for i = rows:-1:1
+                person = aisle(i,:);
+                % Tehdaan toimenpide jonottavalle henkilolle
+                if(person(1) == i)             
+                    % - Tarkistetaan onko henkilo oikealla rivilla
+                    % - Lasketaan henkilolle istuuntumisaika, jos istuuntuminen
+                    % ei ole jo valmis.
+                    % - Asetetaan henkilo paikalleen
+                    % - Poistetaan henkilo kaytavalta
+                    if (odotus(i, 1) == 0)
+                        odotus(i, 1) = 1;
+                        odotus(i, 2) = determineTime(time_step, person, plane(i, :));
+                        wait_map(person(1),person(2)) = wait_map(person(1),person(2))...
+                                                    + odotus(i,2);
+                    end
+                    if (odotus(i, (1:2)) == [1, 0])
+                        plane(person(1), person(2)) = indToSeat(person, seats);
+                        aisle(i,:) = zeros(1,size(aisle,2));
+                        odotus(i, :) = zeros(1,size(odotus,2));
+                    end
+                elseif((i ~= rows) && (person(1) ~= 0))
+                    if (aisle(i + 1, 1) == 0)
+                    % - Jos han ei ole oikealla rivilla -> siirretaan eteenpain
+                    % - Siirretaan henkilo eteenpain
+                        aisle(i+1,:) = person;
+                        aisle(i,:) = zeros(1,size(aisle,2));
+                    end
+                    % veikkaisin, ett? tama olisi oikea kohta ks.
+                    % lisaykselle
+                    wait_map(person(1),person(2)) = wait_map(person(1),person(2))...
+                                                               +time_step;
+                end
+            end
+            % Jos kaytavan ensimmainen paikka on tyhja niin jonottaja ulkoa
+            % voi tulla paastetaan kaytavalle. Poistetaan kokonainen sarake
+            % ulkojonosta.
+            if(any(any(lineIn)) && aisle(1,1) == 0)
+                aisle(1,:) = lineIn(1,:);
+                lineIn(1,:) = [];
+            end
+
+            % poistetaan time_step odotusajasta jokaiselta odottavalta rivilta:
+            odotus(:,2) = odotus(:,2) - time_step*(odotus(:,1) == 1);
+            % muutetaan kaikki negatiiviset ajat nolliksi
+            odotus(:,2) = (odotus(:,2) > 0).*odotus(:,2);
+            % kasvatetaan kulunutta aikaa ja kierrosmaaraa
+            time = time + time_step;
+            aisle;
+            odotus;
+            
+            % Tama suoritetaan jokaisen while-kierroksen jalkeen:
+            varargout{1} = wait_map;
+            temp = [plane(:,1:seats/2), aisle(:, 1), plane(:,seats/2+1:end)];
+            [pcols prows] = find(temp > 0);
+            people.XData = pcols + 0.5;
+            people.YData = -(prows + 0.5);
+            drawnow
+            pause(0.05)
+            %
+        end
+        hold off
+    end
+    plane;
+    time = time/time_step;
+    % Tama suoritetaan jokaisen simulaation jalkeen:
+    % ulos saa tavaraaa varargout vektorin avulla:
+    
+    %
+end
